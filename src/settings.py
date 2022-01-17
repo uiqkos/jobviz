96a001436009d24136344bcbdafe832b1f2d5148
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-01-17 17:44:36 +0300

    добавлены параметры из .env

--- src/settings.py
+++ src/settings.py
@@ -1,39 +1,25 @@
 import pathlib
-from os.path import join
 
 import dotenv
-from pymongo.database import Database
 from pymongo import MongoClient
-
 
 SRC_PATH = pathlib.Path(__file__).parent.absolute()
 ROOT_PATH = pathlib.Path(__file__).parent.parent.absolute()
 
 _dotenv_path = dotenv.find_dotenv()
 
-db_host = dotenv.get_key(_dotenv_path, 'db.host')
-db_port = int(dotenv.get_key(_dotenv_path, 'db.port'))
-db_name = dotenv.get_key(_dotenv_path, 'db.name')
+DB_HOST = dotenv.get_key(_dotenv_path, 'db.host')
+DB_PORT = int(dotenv.get_key(_dotenv_path, 'db.port'))
+DB_NAME = dotenv.get_key(_dotenv_path, 'db.name')
 
-MAX_DESCRIPTION_WORD_COUNT = 1200
-MAX_NAME_WORD_COUNT = 30
-MAX_SPECIALIZATION_COUNT = 30
-MAX_KEY_SKILL_COUNT = 30
-MAX_PROFESSIONALS_ROLE_COUNT = 30
-DEFAULT_CURRENCY_ID = 6  # RUR
+MAX_DESCRIPTION_WORD_COUNT = int(dotenv.get_key(_dotenv_path, "max-description-word-count"))
+MAX_NAME_WORD_COUNT = int(dotenv.get_key(_dotenv_path, "max-name-word-count"))
+MAX_SPECIALIZATION_COUNT = int(dotenv.get_key(_dotenv_path, "max-specialization-count"))
+MAX_KEY_SKILL_COUNT = int(dotenv.get_key(_dotenv_path, "max-key-skill-count"))
+MAX_PROFESSIONALS_ROLE_COUNT = int(dotenv.get_key(_dotenv_path, "max-professionals-role-count"))
+DEFAULT_CURRENCY_ID = int(dotenv.get_key(_dotenv_path, 'default-currency-id'))
 
 db = MongoClient(
-    host=db_host,
-    port=db_port
-).get_database(db_name)
-
-# def get_mongo_client() -> MongoClient:
-#     return MongoClient(
-#         host=db_host,
-#         port=db_port
-#     )
-#
-#
-# def get_db() -> Database:
-#     mc = get_mongo_client()
-#     return mc.get_database(db_name)
+    host=DB_HOST,
+    port=DB_PORT
+).get_database(DB_NAME)
